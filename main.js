c5875ebfafbf616f9c2cbe4b198d57a0d0f323b1
Khadiza Rafa <khadiza301rafa@gmail.com>
2020-08-17 02:59:06 +0600

    Fixed Delete Button

--- main.js
+++ main.js
@@ -31,8 +31,9 @@ const closeIssue = id => {
 
 const deleteIssue = id => {
   const issues = JSON.parse(localStorage.getItem('issues'));
-  const remainingIssues = issues.filter( issue.id !== id )
+  const remainingIssues = issues.filter(issues =>(parseInt(issues.id)!= id))
   localStorage.setItem('issues', JSON.stringify(remainingIssues));
+  fetchIssues();
 }
 
 const fetchIssues = () => {
